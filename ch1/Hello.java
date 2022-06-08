9582ca07ca9acecfea266eeaf78bf5d942731a56
kellcore <48416779+kellcore@users.noreply.github.com>
2022-06-08 14:11:16 -0400

    initial commit

---
+++ ch1/Hello.java
@@ -0,0 +1,7 @@
+public class Hello{
+    public static void main(String[] args) {
+        // this is an inline comment
+        System.out.println("Hello world!");
+        System.out.println("How are you?");
+    }
+}
